b1c223479babad9d02e9e546a2fd899a167770e2
bschirmer <bmschirmer@gmail.com>
2020-07-05 14:57:17 +1000

    Refactor: refactored reviews into functional component; Added hooks

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,93 +1,59 @@
-import React, { Component} from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import reviewsData from "../../resources/reviewsData";
 
-class Reviews extends Component{
-    constructor(){
-        super();
+const Reviews = () => {
 
-        this.state = {
-            author: reviewsData[0].name,
-            job: reviewsData[0].job,
-            info: reviewsData[0].img,
-            image: reviewsData[0].text,
-            index: 0
-        }
-    }    
+    const [reviewNumber, setReviewNumber] = useState(0);
+    const [review, setReview] = useState({image: reviewsData[0].img,
+                                         author: reviewsData[0].author, 
+                                         job: reviewsData[0].job,
+                                         info: reviewsData[0].text});
 
-    componentDidMount(){
-        const prevBtn = document.querySelector(".prev");
-        const nextBtn = document.querySelector(".next");
-        const shuffleBtn = document.querySelector(".shuffle");
-
-        prevBtn.addEventListener('click', () => {
-            let i = this.state.index;
-            if(i == 0) 
-                i = 3;
-            else 
-                i--;
-            
-            this.setState({index: i});
-            this.setState({author: reviewsData[i].name});
-            this.setState({job: reviewsData[i].job});
-            this.setState({image: reviewsData[i].img});
-            this.setState({info: reviewsData[i].text});
-        });
-
-        nextBtn.addEventListener('click', () => {
-            let i = this.state.index;
-            if(i == 3) 
-                i = 0;
-            else 
-                i++;
-            
-            this.setState({index: i});
-            this.setState({author: reviewsData[i].name});
-            this.setState({job: reviewsData[i].job});
-            this.setState({image: reviewsData[i].img});
-            this.setState({info: reviewsData[i].text});
-        });
-
-        shuffleBtn.addEventListener('click', () => {
-            let i = Math.floor(Math.random()*reviewsData.length)
-            
-            this.setState({index: i});
-            this.setState({author: reviewsData[i].name});
-            this.setState({job: reviewsData[i].job});
-            this.setState({image: reviewsData[i].img});
-            this.setState({info: reviewsData[i].text});
-        });
-
-
+    function prevReview() {
+        reviewNumber - 1 < 0 ? setReviewNumber(reviewsData.length - 1) : setReviewNumber(reviewNumber - 1);
     }
 
-    render(){
-        return(
-            <main>
-                <div className="reviews">
-                    <div className="title"> 
-                        Reviews
-                        <span className="underline"></span>
-                    </div>
-                    <article  className="review">
-                        <div className="img-container">
-                            <img src={this.state.image} id="review-image"/>
-                        </div>
-                        <h4 id="author">{this.state.author}</h4>
-                        <p id="job">{this.state.job}</p>
-                        <p id="info">{this.state.info}</p>
-                    </article >
-                    <div>
-                        <Button className="prev"><FontAwesomeIcon icon={faArrowLeft}/></Button>
-                        <Button className="next"><FontAwesomeIcon icon={faArrowRight}/></Button>
-                    </div>
-                    <Button className="shuffle">I'm feelin' lucky</Button>
+    function nextReview() {
+        reviewNumber + 1 > reviewsData.length - 1 ? setReviewNumber(0) : setReviewNumber(reviewNumber + 1);
+    }
+
+    function shuffle() {
+        setReviewNumber(Math.floor(Math.random()*reviewsData.length));
+    }
+
+    useEffect(() => {
+        setReview({ image: reviewsData[reviewNumber].img,
+                    author: reviewsData[reviewNumber].author, 
+                    job: reviewsData[reviewNumber].job,
+                    info: reviewsData[reviewNumber].text});
+    }, [reviewNumber]);
+
+    return(
+        <main>
+            <div className="reviews">
+                <div className="title"> 
+                    Reviews
+                    <span className="underline"></span>
                 </div>
-            </main>
-        );
-    };
+                <article  className="review">
+                    <div className="img-container">
+                        <img src={review.image} id="review-image"/>
+                    </div>
+                    <h4 id="author">{review.author}</h4>
+                    <p id="job">{review.job}</p>
+                    <p id="info">{review.info}</p>
+                </article >
+                <div>
+                    <Button className="prev" onClick={() => prevReview()}><FontAwesomeIcon icon={faArrowLeft}/></Button>
+                    <Button className="next" onClick={() => nextReview()}><FontAwesomeIcon icon={faArrowRight}/></Button>
+                </div>
+                <Button className="shuffle" onClick={() => shuffle()}>I'm feelin' lucky</Button>
+            </div>
+        </main>
+    );
 }
 
 export default Reviews;
