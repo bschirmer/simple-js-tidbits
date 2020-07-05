23bfe47b1151eb42da10848b53cc84fec729ce22
bschirmer <bmschirmer@gmail.com>
2020-07-05 15:33:17 +1000

    Refactor: code clean up

--- src/components/sideBar/sideBar.js
+++ src/components/sideBar/sideBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Button } from '@material-ui/core'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHamburger} from "@fortawesome/free-solid-svg-icons";
